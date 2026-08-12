70d917ae46571ca161acd2f90f8687cde9c1bc29
yntro <milaknis.mar@gmail.com>
2026-08-13 01:25:34 +0300

    imported python-dotenv. fixed dependencies for os.environ.get. updated requirements.txt

--- autoservisas/settings.py
+++ autoservisas/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
