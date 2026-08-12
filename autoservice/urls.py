b4053776620436992f59e5753b715e56d988a0e4
yntro <milaknis.mar@gmail.com>
2026-08-13 00:29:35 +0300

    adjusted according to 5th task 8/12/2026

--- autoservice/urls.py
+++ autoservice/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="autoservice"),
+    path("services/", views.services, name="services"),
+    path("cars/", views.CarListView.as_view(), name="car_list"),
+    path("cars/<int:pk>/", views.CarDetailView.as_view(), name="car_details"),
+    path("orders/", views.order_list, name="order_list"),
+    path("orders/<int:pk>/", views.order_details, name="order_details"),
 ]
--- autoservice/views.py
+++ autoservice/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import View, generic
 
 from autoservice.models import Car, Service, Order, OrderLine
 
@@ -15,3 +16,34 @@ def index(request):
                "completed_orders": completed_orders
                }
     return render(request, "index.html", context)
+
+def services(request):
+    services = Service.objects.all()
+    context = {"services": services}
+    return render(request, "services.html", context)
+
+class CarListView(generic.ListView):
+    model = Car
+    template_name = "car_list.html"
+    context_object_name = "car_list"
+
+class CarDetailView(generic.DetailView):
+    model = Car
+    template_name = "car_details.html"
+    context_object_name = "car"
+
+def order_list(request):
+    completed_orders = sum(order.status for order in Order.objects.all())
+    context = {"completed_orders": completed_orders,
+               "order_list": Order.objects.all(),
+               "order_lines": OrderLine.objects.all(),
+    }
+    return render(request, "order_list.html", context)
+
+def order_details(request, pk):
+    order = get_object_or_404(Order, pk=pk)
+    order_lines = order.orderline_set.all()
+    context = {"order": order,
+               "order_lines": order_lines,
+               }
+    return render(request, "order_details.html", context)
